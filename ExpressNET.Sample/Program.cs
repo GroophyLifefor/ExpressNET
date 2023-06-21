a6401b1f96e634c60c435f11abae55a976de9e8f
Groophy <groophylifefor@gmail.com>
2023-06-21 13:22:28 +0300

    template configurations are added to Dotnet.

---
+++ ExpressNET.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExpressNET;
+
+Express app = new Express();
+app.port = 3000;
+
+app.staticDir("public");
+
+app.route("/", (req, res) =>
+{
+    res.sendFile("views/index.html");
+});
+
+app.listen(port => Console.WriteLine($"Server was running at http://localhost:{port}"));
